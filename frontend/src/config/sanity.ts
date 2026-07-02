46fca94fd92c614d9cc12735cd338d9d73bc8ffc
Wewe2098 <s.mkwatheni@gmail.com>
2026-07-02 15:43:01 +0200

    Fix: disable CDN and add CORS for production Sanity queries

--- frontend/src/config/sanity.ts
+++ frontend/src/config/sanity.ts
@@ -4,7 +4,7 @@ export const sanityClient = createClient({
   projectId: 'y7j5j2mt',
   dataset: 'production',
   apiVersion: '2024-01-01',
-  useCdn: true,
+  useCdn: false,
 })
 
 export const sanityConfig = {
